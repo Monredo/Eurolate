fa66caa047d088cdddcfc440284678989b6549e8
Monredo <107286254+Monredo@users.noreply.github.com>
2022-06-28 11:29:15 -0400

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,18 @@
+const countries = {
+    "en-GB": "English",
+    "es-ES": "Spanish",
+    "fr-FR": "French",
+    "pt-PT": "Portuguese",
+    "de-DE": "German",
+    "it-IT": "Italian",
+    "fi-FI": "Finnish",
+    "el-GR": "Greek",
+    "ru-RU": "Russian",
+    "sv-SE": "Swedish",
+    "tr-TR": "Turkish",
+    "no-NO": "Norwegian",
+    "ga-IE": "Irish",
+    "pl-PL": "Polish",
+    "lb-LU": "Luxembourgish",
+    "ca-ES": "Catalan"
+}
